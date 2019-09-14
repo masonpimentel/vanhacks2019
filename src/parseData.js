b28789c614f98715585a38b3b3a9bee1fb65ce67
Victor Sze <victors@ensemble.com>
2019-09-14 15:44:35 -0700

    added fetchUsers() and fetchCampaigns()

--- src/parseData.js
+++ src/parseData.js
@@ -1,5 +1,26 @@
 import data from './data.json'
 
+var users = []
+var campaigns = []
+
+function fetchUsers () {
+  for (let i = 0; i < data.length; i++) {
+    users[i] = data[i].email;
+  }
+}
+
+function fetchCampaigns () {
+  let keys = Object.keys(data[0])
+  console.log(keys)
+  for (let i = 0; i < keys.length - 1; i++) {
+    campaigns[i] = keys[i + 1]
+  }
+}
+
+fetchUsers()
+fetchCampaigns()
+console.log(users)
+console.log(campaigns)
 console.log(data);
 
 let matrix = [];
