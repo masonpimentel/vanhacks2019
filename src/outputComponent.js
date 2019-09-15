0a412a304995a44f5f1a18aac1e57d453d831834
Mason Pimentel <masonp@ensemble.com>
2019-09-14 18:11:20 -0700

    Create output table

--- src/outputComponent.js
+++ src/outputComponent.js
@@ -2,12 +2,41 @@ import React, { Component } from 'react';
 import './outputComponent.css';
 
 class OutputComponent extends Component {
+    res = [
+        {
+            "email": "user1@d.com",
+            "probability": .45
+        },
+        {
+            "email": "user2@d.com",
+            "probability": .32
+        }
+    ];
+
     render() {
+
+
+        const rows = this.res.map((el, i) =>
+            <tr key={i}>
+                <td>
+                    {el.email}
+                </td>
+                <td>
+                    {el.probability}
+                </td>
+            </tr>
+        );
+
         return (
         <div className="OutputComponent">
             <div className="Output-header">
                 <h2>Output Component</h2>
             </div>
+            <table>
+                <tbody>
+                {rows}
+                </tbody>
+            </table>
         </div>
         );
     }
