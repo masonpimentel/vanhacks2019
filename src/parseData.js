32fcc2d6fe88a041eaf1eebcf9bf7c0a7075e035
Mason Pimentel <masonp@ensemble.com>
2019-09-14 19:53:20 -0700

    Fix parseData

--- src/parseData.js
+++ src/parseData.js
@@ -1,10 +1,8 @@
 import data from './data.json'
 
-var users = []
-var campaigns = []
-let matrix = [];
-
 export function fetchUsers () {
+  var users = [];
+
   for (let i = 0; i < data.length; i++) {
     users[i] = data[i].email;
   }
@@ -12,6 +10,8 @@ export function fetchUsers () {
 }
 
 export function fetchCampaigns () {
+  var campaigns = [];
+
   let keys = Object.keys(data[0])
   for (let i = 0; i < keys.length - 1; i++) {
     campaigns[i] = keys[i + 1]
@@ -20,6 +20,8 @@ export function fetchCampaigns () {
 }
 
 export function fetchMatrix () {
+  let matrix = [];
+
   for (let i = 0; i < data.length; i++) {
     let person = [];
     for (let campaign in data[i]) {
@@ -32,14 +34,8 @@ export function fetchMatrix () {
         }
       }
     }
+    console.log("creating person");
     matrix.push(person);
   }
   return matrix
 }
-
-fetchUsers()
-fetchCampaigns()
-fetchMatrix()
-console.log(users)
-console.log(campaigns)
-console.log(matrix)
